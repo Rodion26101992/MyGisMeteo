e2882d95647af30deb0d2c4e2f182acc93e28e6f
Rodion26101992 <malyshev26101992rodion@gmail.com>
2019-11-12 13:23:46 +0200

    first commit

---
+++ src/redux/reducers/weatherReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case '': {
+            return {}
+        }
+        default:
+            return state
+    }
+
+}
